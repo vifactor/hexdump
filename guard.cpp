21ba3700d7bfd3ac4bd28244e566f3dcb5c9febe
Viktor Kopp <viktor.kopp@mercedes-benz.com>
2024-09-13 20:51:55 +0200

    Coro guard

--- guard.cpp
+++ guard.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+#include <coroutine>
+#include <type_traits>
 
 template<typename T, typename U>
 struct TwoLambdasGuard {
@@ -14,6 +16,35 @@ private:
     U _onExit;
 };
 
+struct nothing {};
+struct GuardEngine {
+    struct promise_type {
+        std::suspend_never initial_suspend() { return {}; }
+        std::suspend_always final_suspend() noexcept { return {}; }
+        void unhandled_exception() {}
+        std::suspend_always yield_value(nothing) { return {}; }
+        GuardEngine get_return_object() {
+            return {std::coroutine_handle<promise_type>::from_promise(*this)};
+        }
+    };
+    
+    void resume() {
+        handle();
+    }
+
+    std::coroutine_handle<promise_type> handle;
+};
+
+template<typename Coro>
+struct CoroGuard {
+    CoroGuard(Coro&& _coro): coro(std::forward<Coro>(_coro)()) {
+    };
+    
+    ~CoroGuard() {
+        coro.resume();
+    }
+    std::invoke_result_t<Coro> coro;
+};
 
 int main() {
     {
@@ -23,6 +54,17 @@ int main() {
             std::cout << "On Exit" << std::endl;
         });
 
+        std::cout << "Some code" << std::endl;
+    }
+
+    std::cout << "-----------" << std::endl;
+    {
+        CoroGuard guard([]() -> GuardEngine {
+            std::cout << "On enter" << std::endl;
+            co_yield {};
+            std::cout << "On exit" << std::endl;
+            }
+        );
         std::cout << "Some code" << std::endl;
     }
 
